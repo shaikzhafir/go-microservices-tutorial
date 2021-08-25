fbe7a40e32d45c3272edcbb0822106beffdc6aa8
shaik-zhafir <shaik_zhafir@dell.com>
2021-08-25 22:29:46 +0800

    ommited the graceful shutdown part and the signals part (feel like abit much for now

--- main.go
+++ main.go
@@ -7,16 +7,30 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	//returns as a Hello struct?
+	//each handler is returning the struct, which is like a route
+
 	hh := handlers.NewHello(l)
 	gb := handlers.NewGoodbye(l)
+
+	//server multiplexer is the handler
 	sm := http.NewServeMux()
 	sm.Handle("/goodbye", gb)
 	sm.Handle("/", hh)
 
-	http.ListenAndServe(":8080", sm)
+	//this is to prevent stuff like dos
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	s.ListenAndServe()
+
 }
